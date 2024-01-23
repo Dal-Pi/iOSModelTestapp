6de0e1f43837b0baca7abe6aeba74ef7467f033f
Dal-Pi <kimsh707@naver.com>
2024-01-23 14:27:32 +0900

    impl face obejct detection

--- iOSModelTestapp/iOSModelTestapp/ViewController.swift
+++ iOSModelTestapp/iOSModelTestapp/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     @IBOutlet weak var previewView: UIView!
@@ -44,10 +45,46 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 
         DispatchQueue.global().async {
             do {
-                try self.cameraDevice?.openCamera(cameraType: .builtInWideAngleCamera, position: .back)
+                try self.cameraDevice?.openCamera(cameraType: .builtInWideAngleCamera, position: .front)
             } catch {
                 print("error while opening camera")
             }
+        }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            print("error while CMSampleBufferGetImageBuffer")
+            return
+        }
+
+        let ciImage = CIImage(cvPixelBuffer: imageBuffer).oriented(.right)
+
+        analyzeImage(image: ciImage)
+    }
+
+    func analyzeImage(image: CIImage) {
+        let handler = VNImageRequestHandler(ciImage: image, orientation: .up)
+        let request = VNDetectFaceRectanglesRequest { requested, error in
+            guard let observations = requested.results as? [VNFaceObservation] else {
+                print("there is no observations")
+                return
+            }
+            let _bestObservation = observations.max { lhs, rhs in
+                lhs.confidence < rhs.confidence
+            }
+            guard let bestObservation = _bestObservation else {
+                print("there is no bestObservation")
+                return
+            }
+
+            print("bestObservation box : \(String(describing: bestObservation.boundingBox))")
+            //TODO draw rect
+        }
+        do {
+            try handler.perform([request])
+        } catch {
+            print("error while perform request")
         }
     }
 }
